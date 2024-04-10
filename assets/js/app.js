7b68b9e6b637225399e8df4620cb97a86ac9bdeb
esepgit <esep.bits@gmail.com>
2024-04-09 20:26:20 -0400

    page finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,24 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 4000,
   touch: false,
 });
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function () {
+  $("#card-2").click(function () {
+    $("#card-2-img").css({
+      transform: "rotateY(180deg)"
+    })
+  });
+
+  $("#card-3").dblclick(function () {
+    $("#card-3-title").css({
+      color: "black"
+    });
+  });
+
+  $(".recipe-title").click(function () {
+      $(".recipe-text").toggle();
+  })
+});
